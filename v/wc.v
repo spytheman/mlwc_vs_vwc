2696b30bfa9cec95333d1d585edc8828d45f3b5f
Delyan Angelov <delian66@gmail.com>
2019-11-29 21:31:14 +0200

    use C.isspace and C.isprint . simplify state structure.

--- v/wc.v
+++ v/wc.v
@@ -1,15 +1,16 @@
 import os
 
+fn C.isspace(int) int
+fn C.isprint(int) int
+
 struct State {
 mut:
   was_nl bool
-  was_space bool
-  was_alnum bool
+  was_wordchar bool
 }
 
 [inline] fn is_nl(c byte)    bool { return (c == `\n`) }
-[inline] fn is_ws(c byte)    bool { return (c == ` ` || c == `\t` ) }
-[inline] fn is_alnum(c byte) bool { return (c >= `a` && c <= `z`) || (c>=`A` && c<= `Z`) }
+[inline] fn is_wordchar(c byte) bool { return C.isprint(c)!=0 && C.isspace(c)==0 }
 
 fn main(){
   data := os.read_file(os.args[1]) or { panic(err) }
@@ -17,16 +18,14 @@ fn main(){
   
   total := data.len
   mut newlines := 0
-  mut spaces := 0 
   mut words := 0
   
   mut ostate := State{ was_nl: true }
   for i := 0; i < total; i++ {
       c := chars[i]
-      nstate := State{ was_nl: is_nl(c) was_space: is_ws(c) was_alnum: is_alnum(c) }
+      nstate := State{ was_nl: is_nl(c) was_wordchar: is_wordchar(c) }
       if nstate.was_nl { newlines++ }
-      if nstate.was_space { spaces++ }
-      if !ostate.was_alnum && nstate.was_alnum  { words++ }
+      if !ostate.was_wordchar && nstate.was_wordchar { words++ }
       ostate = nstate
   }
   
