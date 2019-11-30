f00296722e94281317fcb8dce63f39fbe8e51979
Delyan Angelov <delian66@gmail.com>
2019-11-30 08:18:08 +0200

    Cleanup the v code even more, slightly reduced performance for now, but much more readable.

--- v/wc.v
+++ v/wc.v
@@ -13,15 +13,13 @@ mut:
 fn main(){
   file := os.args[1]
   data := os.read_file(file) or { panic(err) }
-  chars := &byte(data.str)
   
   total := data.len
   mut newlines := 0
   mut words := 0
   
   mut ostate := State{ was_nl: true }
-  for i := 0; i < total; i++ {
-      c := chars[i]
+  for c in data {
       nstate := State{ was_nl: is_nl(c) was_wordchar: is_wordchar(c) }
       if nstate.was_nl { newlines++ }
       if !ostate.was_wordchar && nstate.was_wordchar { words++ }
